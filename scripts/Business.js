3cbe60edebd9ed4173784dba73787c77a14dbdf6
Glenn Dixon Jr <steve@stevenbrownlee.com>
2021-11-20 09:12:00 -0600

    added the css to the html tag

--- scripts/Business.js
+++ scripts/Business.js
@@ -2,9 +2,14 @@
 export const Business = (businessObject) => {
     return `
         <section class="business">
-            <h2 class="business_type">${businessObject.companyName}</h2>
+            <h1 class="business_type">${businessObject.companyName}</h1>
             <div class="business_address">
-                Address: ${businessObject.addressFullStreet}  
+                <p class="address_fullstreet">
+                    ${businessObject.addressFullStreet}
+                </p>
+                <p class="address_state">
+                    ${businessObject.addressCity}, ${businessObject.addressStateCode} ${businessObject.addressZipCode}
+                </p>
             </div>
         </section>
     `
--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,17 +1,16 @@
 import { getBusinesses } from "./database.js";
 import { Business } from "./Business.js"
 
-const contentTarget = document.querySelector("#container")
+let contentTarget = document.querySelector("#container")
 
 export const BusinessList = () => {
     const businessesArray = getBusinesses()
-    contentTarget.innerHTML = "<h1>Active Business</h1>"
     contentTarget.innerHTML = "<ul>"
     businessesArray.forEach(
         (businessObject) => {
             contentTarget.innerHTML += `<li>${Business(businessObject)}</li>`
         }
-    );
+        );
     contentTarget.innerHTML += "</ul>"
     return contentTarget.innerHTML
 }
